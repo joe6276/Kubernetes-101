5d04433a7ce2da65a13227863aff9d1cdea531b6
Jonathan <joendambuki16@gmail.com>
2024-08-14 11:21:29 +0300

    Kubernetes Getting Started Project

---
+++ Kubernetes Projects/1. Getting-Started-with-Kubernetes/app.js
@@ -0,0 +1,20 @@
+const express= require('express')
+
+const app = express()
+
+
+app.get("/", (req,res)=>{
+    res.send(`     
+        <h1> Hello Kubernetes </h1>
+        `)
+})
+
+app.get('/error', (req,res)=>{
+    process.exit(1)
+})
+
+
+app.listen(80, ()=>{
+    console.log("App is Running...");
+    
+})
