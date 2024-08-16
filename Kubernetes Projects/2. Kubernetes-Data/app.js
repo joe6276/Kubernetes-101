8aedb0016a64ff652aca5ef313447cf242775c97
Jonathan <joendambuki16@gmail.com>
2024-08-16 05:09:52 +0300

    Worked on node storage Project

---
+++ Kubernetes Projects/2. Kubernetes-Data/app.js
@@ -0,0 +1,38 @@
+const path= require('path')
+const fs= require('fs')
+const express= require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const filePath = path.join(__dirname, 'students', 'text.txt')
+
+
+app.get('/student', (req,res)=>{
+    fs.readFile(filePath, (err,data)=>{
+        if(err){
+            return res.status(500).json({message:' Failed to open file.'})
+        }
+        res.status(200).json({student: data.toString()})
+    })
+})
+
+app.post('/student', (req,res)=>{
+    const newText= req.body.text;
+    if(newText.trim().length ===0){
+        res.status(422).json({message:'Student name must not be Empty'})
+    }
+    fs.appendFile(filePath, newText+ '\n', (err)=>{
+        if(err){
+            return res.status(500).json({message:'Failed to store text'})
+        }
+        res.status(200).json({message:' Student stored!!'})
+    })
+})
+
+app.get('/error', (req,res)=>{
+    process.exit(1)
+})
+
+app.listen(80)
